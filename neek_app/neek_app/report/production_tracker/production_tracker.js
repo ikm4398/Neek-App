b06982b4feca3ef5956a9022178e53a624b44deb
Indra Kumar Mehta <ikm4398@gmail.com>
2026-07-27 05:34:26 +0000

    add report core module to custom app

---
+++ neek_app/neek_app/report/production_tracker/production_tracker.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2026, a and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Production Tracker"] = {
+	"filters": [
+
+	]
+};
